88d89aa6b97fcef08ceeccf820e41c86687dce95
Hopd84 <Ca_dict@mail.ru>
2022-06-09 10:00:30 +0300

    Revert "Added few modules."
    This reverts commit f29d7816db1b712301ce5a4feefb7af0a671868e.

--- src/main/java/com/market/home/ApplicationStarter.java
+++ src/main/java/com/market/home/ApplicationStarter.java
@@ -1,11 +1,3 @@
 package com.market.home;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ApplicationStarter {
-    public static void main(String[] args) {
-        SpringApplication.run(ApplicationStarter.class, args);
-    }
-}
+public class ApplicationStarter {}
