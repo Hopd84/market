6805141bcb7e48379a84cb1e9b1fdca4ad159114
Hopd84 <Ca_dict@mail.ru>
2022-06-09 10:03:21 +0300

    Added main() into ApplicationStarter

--- src/main/java/com/market/home/ApplicationStarter.java
+++ src/main/java/com/market/home/ApplicationStarter.java
@@ -1,3 +1,11 @@
 package com.market.home;
 
-public class ApplicationStarter {}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+}
